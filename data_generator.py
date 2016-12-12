f24993e458dd2a43eb6e38bbe49f677269095856
Alex Mattson <alexmattson@Alexs-MBP.lan>
2016-12-12 09:32:52 -0800

    infinite data generator

--- data_generator.py
+++ data_generator.py
@@ -9,31 +9,24 @@ import json
 devices = ["aaaa", "bbbb", "cccc", "dddd", "eeee"]
 delivery_stream_id = "hhhhgggg-DataKinesisFirehose-1RKTJYBTNYXZO"
 
-# random.randint(-5,5)
-
 client = boto3.client('firehose')
-'''
-response = client.put_record_batch(
-    DeliveryStreamName=delivery_stream_id,
-    Records=[
-        {
-            'Data': b'bytes'
-        },
-    ]
-)
-'''
 
-# simulate each device sending its telemetry data
-for device in devices:
-    payload = {
-        'deviceId' : device,
-        'sensorReading' : random.randint(-5,5),
-        'readingTimestamp' : datetime.datetime.utcnow().isoformat()
-    }
-    print(client.put_record(
-        DeliveryStreamName=delivery_stream_id,
-        Record={
-            'Data': json.dumps(payload)
+# run forever
+while True:
+
+    # simulate each device sending its telemetry data
+    for device in devices:
+        payload = {
+            'deviceId' : device,
+            'sensorReading' : random.randint(-5,5),
+            'readingTimestamp' : datetime.datetime.utcnow().isoformat()
         }
+        print(client.put_record(
+            DeliveryStreamName=delivery_stream_id,
+            Record={
+                'Data': json.dumps(payload)
+            }
+            )
         )
-    )
+
+    time.sleep(5)
