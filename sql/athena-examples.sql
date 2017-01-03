3c611d9476f8f4370f333e9b108b38faa69464fe
Alex Mattson <alexmattson@Alexs-MBP-2.lan>
2017-01-02 23:16:58 -0800

    updated table & database name

--- sql/athena-examples.sql
+++ sql/athena-examples.sql
@@ -1,17 +1,17 @@
 -- a small collection of really simple queries to run against your data for this demo
--- in these examples my database is named 'anomaly' and my table is named 'a2'
+-- in these examples my database is named 'analyticsdemo' and my table is named 'devicedata'
 
 -- find entries outside of the expected -5 to 5 range
-SELECT * FROM anomaly.a2 WHERE sensorreading < -5 or sensorreading > 5;
+SELECT * FROM analyticsdemo.devicedata WHERE sensorreading < -5 or sensorreading > 5;
 
 -- count # of records by deviceId
-SELECT COUNT(*) AS records FROM anomaly.a2 GROUP BY deviceid;
+SELECT COUNT(*) AS records FROM analyticsdemo.devicedata GROUP BY deviceid;
 
 -- find max reading by deviceId
-SELECT deviceid, MAX(sensorreading) AS maxreading FROM anomaly.a2 GROUP BY deviceid;
+SELECT deviceid, MAX(sensorreading) AS maxreading FROM analyticsdemo.devicedata GROUP BY deviceid;
 
 -- find min reading by deviceId
-SELECT deviceid, MIN(sensorreading) AS minreading FROM anomaly.a2 GROUP BY deviceid;
+SELECT deviceid, MIN(sensorreading) AS minreading FROM analyticsdemo.devicedata GROUP BY deviceid;
 
 -- find average reading by deviceId
-SELECT deviceid, AVG(sensorreading) AS avgreading FROM anomaly.a2 GROUP BY deviceid;
+SELECT deviceid, AVG(sensorreading) AS avgreading FROM analyticsdemo.devicedata GROUP BY deviceid;
