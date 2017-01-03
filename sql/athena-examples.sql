e5a000b8afb7ccfbf854c5c3df7f1bd063f19d68
Alex Mattson <alexmattson@Alexs-MBP-2.lan>
2017-01-02 19:34:48 -0800

    updated SQL examples for Athena

--- sql/athena-examples.sql
+++ sql/athena-examples.sql
@@ -1,2 +1,13 @@
--- SELECT * FROM anomaly.a1 WHERE sensorreading ;
-SELECT count(*) as records FROM anomaly.a1 GROUP BY deviceid;
+-- in these examples my database is named 'anomaly' and my table is named 'a2'
+
+-- find entries outside of the expected -5 to 5 range
+SELECT * FROM anomaly.a2 WHERE sensorreading < -5 or sensorreading > 5;
+
+-- count # of records per deviceId
+SELECT COUNT(*) AS records FROM anomaly.a2 GROUP BY deviceid;
+
+-- find max reading per deviceId
+SELECT deviceid, MAX(sensorreading) AS maxreading FROM anomaly.a2 GROUP BY deviceid;
+
+-- find min reading per deviceId
+SELECT deviceid, MIN(sensorreading) AS minreading FROM anomaly.a2 GROUP BY deviceid;
