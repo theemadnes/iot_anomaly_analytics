46daabf53b4a51610a55b312275847689c2d6e62
Alex Mattson <alexmattson@Alexs-MBP-2.lan>
2017-01-02 19:40:01 -0800

    added an AVG example

--- sql/athena-examples.sql
+++ sql/athena-examples.sql
@@ -1,13 +1,17 @@
+-- a small collection of really simple queries to run against your data for this demo
 -- in these examples my database is named 'anomaly' and my table is named 'a2'
 
 -- find entries outside of the expected -5 to 5 range
 SELECT * FROM anomaly.a2 WHERE sensorreading < -5 or sensorreading > 5;
 
--- count # of records per deviceId
+-- count # of records by deviceId
 SELECT COUNT(*) AS records FROM anomaly.a2 GROUP BY deviceid;
 
--- find max reading per deviceId
+-- find max reading by deviceId
 SELECT deviceid, MAX(sensorreading) AS maxreading FROM anomaly.a2 GROUP BY deviceid;
 
--- find min reading per deviceId
+-- find min reading by deviceId
 SELECT deviceid, MIN(sensorreading) AS minreading FROM anomaly.a2 GROUP BY deviceid;
+
+-- find average reading by deviceId
+SELECT deviceid, AVG(sensorreading) AS avgreading FROM anomaly.a2 GROUP BY deviceid;
